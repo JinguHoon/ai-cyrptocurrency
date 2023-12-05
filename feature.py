805073aa645706c8d26dc16c8c6e38067aa3c7ef
이승훈 <61940768+JinguHoon@users.noreply.github.com>
2023-12-05 13:49:35 +0900

    보너스 제외한 코드(기본)

--- feature.py
+++ feature.py
@@ -23,28 +23,26 @@ def cal_book_balance(gr_bid_level, gr_ask_level, mid_price):
     askPx = 0.0
     bidQty = 0.0
     askQty = 0.0
-    keys = gr_bid_level.keys()
     if len(gr_bid_level) > 0 and len(gr_ask_level) > 0:
-        for i in keys:
-            print(keys)
-            #cal_bidQty = pow(gr_bid_level.loc[i].quantity, ratio)
-            #cal_askQty = pow(gr_ask_level.loc[i].quantity, ratio)
-            #bidQty += cal_bidQty 
-            #askQty += cal_askQty
-            #bidPx += cal_bidQty * gr_bid_level.loc[i].price
-            #askPx += cal_askQty * gr_ask_level.loc[i].price
+        for i in range(0,len(gr_bid_level)):
+            cal_bidQty = pow(gr_bid_level.iloc[i].quantity, ratio)
+            cal_askQty = pow(gr_ask_level.iloc[i].quantity, ratio)
+            bidQty += cal_bidQty 
+            askQty += cal_askQty
+            bidPx += cal_bidQty * gr_bid_level.iloc[i].price
+            askPx += cal_askQty * gr_ask_level.iloc[i].price
 
-        #book_price = (((askQty*bidPx)/bidQty) + ((bidQty*askPx)/askQty)) / (bidQty+askQty)
+        book_price = (((askQty*bidPx)/bidQty) + ((bidQty*askPx)/askQty)) / (bidQty+askQty)
 
-        #book_balance = (book_price-mid_price) / interval
-        #return (book_balance)
+        book_balance = (book_price-mid_price) / interval
+        return (book_balance)
 
     else:
         print ('Error: serious cal_book_balance')
         return (-1)    
 
 
-df = pd.read_csv('2023-12-03-bithumb-btc-orderbook.csv').apply(pd.to_numeric,errors='ignore')
+df = pd.read_csv('2023-11-13-bithumb-btc-orderbook.csv').apply(pd.to_numeric,errors='ignore')
 groups = df.groupby('timestamp')
 
 keys = groups.groups.keys()
